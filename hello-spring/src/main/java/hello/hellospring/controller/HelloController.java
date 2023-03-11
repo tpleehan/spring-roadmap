7ec3c9bd70bae70cc2c30ddf5163f1d05e0c41d2
tpleehan <tpleehan@gmail.com>
2023-03-12 00:30:08 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 스프링 웹 개발 기초(MVC와 템플릿 엔진)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,4 +14,10 @@ public class HelloController {
 		return "hello";
 	}
 
+	@GetMapping("hello-mvc")
+	public String helloMVc(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "hello-template";
+	}
 }
