17c1cd4e1f1b991d8a1484ef93427bbd4a644bfe
tpleehan <tpleehan@gmail.com>
2023-03-13 03:18:40 +0900

    스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 회원 관리 예제 - 웹 MVC 개발(회원 웹 기능 - 등록)

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,7 +2,10 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -14,4 +17,20 @@ public class MemberController {
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
+
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping("/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+
+		memberService.join(member);
+
+		return "redirect:/";
+	}
+
 }
