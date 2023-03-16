5bb44c379619b01fe9f85e766945be3a3ae869aa
tpleehan <tpleehan@gmail.com>
2023-03-16 12:16:22 +0900

    스프링 핵심 원리 - 기본편 - 싱글톤 컨테이너(싱글톤 패턴)

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,6 +1,7 @@
 package hello.core.singleton;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +25,18 @@ class SingletonTest {
 		System.out.println("memberService2 = " + memberService2);
 
 		// memberService1 != memberService2
-		Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+		assertThat(memberService1).isNotSameAs(memberService2);
+	}
+
+	@Test
+	@DisplayName("싱글톤 패턴을 적용한 객체 사용")
+	void singletonServiceTest() {
+		SingletonService singletonService1 = SingletonService.getInstance();
+		SingletonService singletonService2 = SingletonService.getInstance();
+
+		System.out.println("singletonService1 = " + singletonService1);
+		System.out.println("singletonService2 = " + singletonService2);
+
+		assertThat(singletonService1).isSameAs(singletonService2);
 	}
 }
