c5e826e200cadf894c5c5b65da4ce4f6969a10ef
tpleehan <tpleehan@gmail.com>
2023-03-18 17:39:45 +0900

    스프링 핵심 원리 - 기본편 - 빈 스코프(프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결)

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,36 +36,18 @@ public class SingletonWithPrototypeTest1 {
 
 		ClientBean clientBean2 = ac.getBean(ClientBean.class);
 		int count2 = clientBean2.logic();
-		assertThat(count2).isEqualTo(2);
+		assertThat(count2).isEqualTo(1);
 
 	}
 
 	@Scope("singleton")
 	static class ClientBean {
-		private final PrototypeBean prototypeBean; // 생성시점에 주입 x01
 
 		@Autowired
-		public ClientBean(PrototypeBean prototypeBean) {
-			this.prototypeBean = prototypeBean;
-		}
+		private Provider<PrototypeBean> prototypeBeanProvider;
 
 		public int logic() {
-			prototypeBean.addCount();
-			int count = prototypeBean.getCount();
-			return count;
-		}
-	}
-
-	@Scope("singleton")
-	static class ClientBean2 {
-		private final PrototypeBean prototypeBean; // 생성시점에 주입 x02
-
-		@Autowired
-		public ClientBean2(PrototypeBean prototypeBean) {
-			this.prototypeBean = prototypeBean;
-		}
-
-		public int logic() {
+			PrototypeBean prototypeBean = prototypeBeanProvider.get();
 			prototypeBean.addCount();
 			int count = prototypeBean.getCount();
 			return count;
