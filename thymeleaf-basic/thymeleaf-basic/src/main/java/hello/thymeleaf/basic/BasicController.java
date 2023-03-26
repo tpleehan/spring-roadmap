61b0a57edf215f7db8c33dd792a1fc65e08f2dfe
tpleehan <tpleehan@gmail.com>
2023-03-26 21:23:24 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 타임리프 - 기본 기능(기본 객체들)

--- thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,6 +49,19 @@ public class BasicController {
 		model.addAttribute("userMap", map);
 
 		return "basic/variable";
+	}
+
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
 	}
 
 	@Data
