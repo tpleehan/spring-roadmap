6df085ad40c885981350ea84fb220ea17bd12466
tpleehan <tpleehan@gmail.com>
2023-03-27 11:32:32 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 메시지, 국제화(스프링 메시지 소스 설정)

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,15 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/*
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new
+			ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+	*/
+
 }
