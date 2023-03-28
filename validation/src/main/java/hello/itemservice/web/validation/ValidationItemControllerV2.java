9191d2fce584ecce158cdc9d0dadb2525add0784
tpleehan <tpleehan@gmail.com>
2023-03-28 21:52:22 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 검증1 - Validation(Validator 분리1)

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
+	private final ItemValidator itemValidator;
 
 	@GetMapping
 	public String items(Model model) {
@@ -180,7 +181,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+	// @PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes, Model model) {
 
@@ -206,6 +207,25 @@ public class ValidationItemControllerV2 {
 			}
 		}
 
+		// 검증에 실패하면 다시 입력 폼으로 이동한다.
+		if (bindingResult.hasErrors()) {
+			log.info("errors = {}", bindingResult);
+			return "validation/v2/addForm";
+		}
+
+		// 성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes, Model model) {
+
+		itemValidator.validate(item, bindingResult);
+		
 		// 검증에 실패하면 다시 입력 폼으로 이동한다.
 		if (bindingResult.hasErrors()) {
 			log.info("errors = {}", bindingResult);
