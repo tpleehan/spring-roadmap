9f61a8333a505ced1bb7cd1051d4cd23f3eb3c33
tpleehan <tpleehan@gmail.com>
2023-03-29 12:03:19 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 검증2 - Bean Validation(Bean Validation - groups)

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,5 +1,6 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -10,18 +11,18 @@ import lombok.Data;
 @Data
 public class Item {
 
-	@NotNull // 수정 요구사항 추가
+	@NotNull(groups = UpdateCheck.class)
 	private Long id;
 
-	@NotBlank(message = "공백X")
+	@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
 	private String itemName;
 
-	@NotNull
-	@Range(min = 1000, max = 1000000)
+	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
 	private Integer price;
 
-	@NotNull
-	// @Max(9999) // 수정 요구사항 추가
+	@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+	@Max(value = 9999, groups = {SaveCheck.class})
 	private Integer quantity;
 
 	public Item() {
