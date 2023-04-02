1d3b77b9e29561f8e811d1267c04d29db4208deb
tpleehan <tpleehan@gmail.com>
2023-04-02 18:26:00 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - 스프링 타입 컨버터(포맷터 적용하기)

--- typeconverter/typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
+++ typeconverter/typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -13,7 +13,7 @@ public class MyNumberFormatter implements Formatter<Number> {
 
 	@Override
 	public Number parse(String text, Locale locale) throws ParseException {
-		log.info("text={}", "locale={}", text, locale);
+		log.info("text={}, locale={}", text, locale);
 		// "1,000" -> 1000
 		NumberFormat format = NumberFormat.getInstance(locale);
 		return format.parse(text);
@@ -21,7 +21,7 @@ public class MyNumberFormatter implements Formatter<Number> {
 
 	@Override
 	public String print(Number object, Locale locale) {
-		log.info("object={}", "locale={}", object, locale);
+		log.info("object={}, locale={}", object, locale);
 		return NumberFormat.getInstance(locale).format(object);
 	}
 }
