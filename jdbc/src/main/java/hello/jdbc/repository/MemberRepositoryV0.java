dca3177b913790b30920f9a2b8969f17b3967b24
tpleehan <tpleehan@gmail.com>
2023-04-07 23:34:06 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 - JDBC 이해(JDBC 개발 - 조회)

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
@@ -36,6 +37,37 @@ public class MemberRepositoryV0 {
 		} finally {
 			close(con, pstmt, null);
 		}
+	}
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from member where member_id = ?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, null);
+		}
+
 	}
 
 	private void close(Connection con, Statement stmt, ResultSet rs) {
