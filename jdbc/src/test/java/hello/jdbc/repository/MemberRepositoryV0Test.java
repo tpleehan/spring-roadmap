5dc5ae19ee23940dbc4d8ac1f230f4afd27ab080
tpleehan <tpleehan@gmail.com>
2023-04-07 23:57:30 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 - JDBC 이해(JDBC 개발 - 수정, 삭제)

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -3,6 +3,7 @@ package hello.jdbc.repository;
 import static org.assertj.core.api.Assertions.*;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +18,7 @@ class MemberRepositoryV0Test {
 	@Test
 	void crud() throws SQLException {
 		// save
-		Member member = new Member("memberV4", 10000);
+		Member member = new Member("memberV100", 10000);
 		repository.save(member);
 
 		// findById
@@ -26,6 +27,16 @@ class MemberRepositoryV0Test {
 		log.info("member == findMember {}", member == findMember);
 		log.info("member equals findMember {}", member.equals(findMember));
 		assertThat(findMember).isEqualTo(member);
+
+		// update: money: 10000 -> 20000
+		repository.update(member.getMemberId(), 20000);
+		Member updateMember = repository.findById(member.getMemberId());
+		assertThat(updateMember.getMoney()).isEqualTo(20000);
+
+		// delete
+		repository.delete(member.getMemberId());
+		assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+			.isInstanceOf(NoSuchElementException.class);
 	}
 
 }
