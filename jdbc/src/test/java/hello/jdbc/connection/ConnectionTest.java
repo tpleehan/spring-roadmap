560c641d9a3a85fa54c7c9a20edf0468f3fdcff1
tpleehan <tpleehan@gmail.com>
2023-04-08 09:28:54 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 - 커넥션풀과 데이터소스 이해(DataSource 예제2 - 커넥션 풀)

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -31,6 +33,20 @@ public class ConnectionTest {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		useDataSource(dataSource);
 
+	}
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		// 커넥션 풀링
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("MyPool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);
 	}
 
 	private void useDataSource(DataSource dataSource) throws SQLException {
