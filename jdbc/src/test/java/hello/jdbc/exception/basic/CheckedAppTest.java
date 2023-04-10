adf3c05603920cf34ed89e2ef5974efee2ba27ed
tpleehan <tpleehan@gmail.com>
2023-04-10 10:19:56 +0900

    스프링 DB 1편 - 데이터 접근 핵심 원리 - 자바 예외 이해(체크 예외 활용)

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,53 @@
+package hello.jdbc.exception.basic;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CheckedAppTest {
+
+	@Test
+	void checked() {
+		Controller controller = new Controller();
+		assertThatThrownBy(() -> controller.request())
+			.isInstanceOf(Exception.class);
+	}
+
+	static class Controller {
+		Service service = new Service();
+
+		public void request() throws SQLException, ConnectException {
+			service.logic();
+		}
+	}
+
+	static class Service {
+		Repository repository = new Repository();
+		NetworkClient networkClient = new NetworkClient();
+
+		public void logic() throws SQLException, ConnectException {
+			repository.call();
+			networkClient.call();
+		}
+
+	}
+
+	static class NetworkClient {
+		public void call() throws ConnectException {
+			throw new ConnectException("연결 실패");
+		}
+	}
+
+	static class Repository {
+		public void call() throws SQLException {
+			throw new SQLException("ex");
+		}
+	}
+
+}
