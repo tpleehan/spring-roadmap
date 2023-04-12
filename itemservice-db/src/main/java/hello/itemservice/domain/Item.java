8b16d7c55d7ebe265bc86a9850f63c9663fc3331
tpleehan <tpleehan@gmail.com>
2023-04-12 11:54:54 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술 - 데이터 접근 기술 - JPA(JPA 적용1 - 개발)

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,22 +1,32 @@
 package hello.itemservice.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
 
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
+	@Column(name = "item_name", length = 10)
+	private String itemName;
+	private Integer price;
+	private Integer quantity;
 
-    public Item() {
-    }
+	public Item() {
+	}
 
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
 }
