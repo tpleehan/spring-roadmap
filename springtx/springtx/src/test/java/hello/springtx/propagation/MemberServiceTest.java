7278a7911a4e2b9511b47b8a149cb0c963bc3d92
tpleehan <tpleehan@gmail.com>
2023-04-16 21:28:15 +0900

    스프링 DB 2편 - 데이터 접근 활용 기술 - 스프링 트랜잭션 전파2 - 활용(트랜잭션 전파 활용2 - 커밋, 롤백)

--- springtx/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -37,6 +38,25 @@ class MemberServiceTest {
 		// then: 모든 데이터가 정상 저장된다.
 		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isPresent());
+	}
+
+	/**
+	 * memberService	@Transactional:OFF
+	 * memberRepository	@Transactional:ON
+	 * logRepository	@Transactional:ON Exception
+	 */
+	@Test
+	void outerTxOff_fail() {
+		// given
+		String username = "로그예외_outerTxOff_fail";
+
+		// when
+		assertThatThrownBy(() -> memberService.joinV1(username))
+			.isInstanceOf(RuntimeException.class);
+
+		// then: 모든 데이터가 정상 저장된다.
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
 	}
 
 }
