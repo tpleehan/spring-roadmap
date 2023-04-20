efc051728b780edc09b2b77de8d9ab6c809f30e1
tpleehan <tpleehan@gmail.com>
2023-04-20 12:52:54 +0900

    스프링 핵심 원리 - 고급편 - 프록시 패턴과 데코레이터 패턴(데코레이터 패턴 - 예제 코드2)

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,7 +2,9 @@ package hello.proxy.pureproxy.decorator;
 
 import org.junit.jupiter.api.Test;
 
+import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +14,14 @@ public class DecoratorPatternTest {
 	void noDecorator() {
 		RealComponent realComponent = new RealComponent();
 		DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+		client.execute();
+	}
+
+	@Test
+	void decorator1() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 		client.execute();
 	}
 }
