6fecad75ff51b3d3474108991e6f5db59048e468
tpleehan <tpleehan@gmail.com>
2023-04-30 18:26:37 +0900

    스프링 부트 - 핵심 원리와 활용 - 외부설정과 프로필2(외부설정 사용 - @ConfigurationProperties 시작)

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -2,13 +2,16 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-import hello.config.MyDataSourceValueConfig;
+import hello.config.MyDataSourceConfigV1;
 
 // @Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+// @Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
+@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
 	public static void main(String[] args) {
