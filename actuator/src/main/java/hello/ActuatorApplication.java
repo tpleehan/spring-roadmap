d7a7665965f69de6bbbaffe0e047c1c9c9bf9fe4
tpleehan <tpleehan@gmail.com>
2023-05-02 13:16:13 +0900

    스프링 부트 - 핵심 원리와 활용 - 모니터링 메트릭 활용(메트릭 등록 - 예제 만들기)

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import hello.order.v0.OrderServiceV0;
+
+@Import(OrderServiceV0.class)
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ActuatorApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
 
-    @Bean
-    public InMemoryHttpExchangeRepository httpExchangeRepository() {
-        return new InMemoryHttpExchangeRepository();
-    }
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 
 }
